764e4af2b2291e791b064406a65507420a717596
Pavol Bakoň <pavol.bakon@atos.net>
2023-04-06 20:22:00 +0200

    lo

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorGitHubPagesDemo;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
