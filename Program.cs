e20862f69a79656d18b4c49a5c379340b5626c5e
Pavol Bakoň <pavol.bakon@atos.net>
2023-04-19 13:52:14 +0200

    watch

--- Program.cs
+++ Program.cs
@@ -3,11 +3,21 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorGitHubPagesDemo;
 using Microsoft.JSInterop;
 using System.Security.AccessControl;
+using ll;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.Add<ll.MyUpdatingComponent>("#counter"); // add the updating component with the CSS selector "#updating-component"
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(ss => new MyUpdatingComponent());
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+// Start the updating component
+var updatingComponent = host.Services.GetRequiredService<ll.MyUpdatingComponent>();
+updatingComponent.StartUpdating();
+
+await host.RunAsync();
