80cb2748566ae6d71a59020bac0f5a0a9f36334d
Pavol Bakoň <pavol.bakon@atos.net>
2023-04-22 13:35:01 +0200

    timer

--- Program.cs
+++ Program.cs
@@ -8,16 +8,16 @@ using ll;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.RootComponents.Add<ll.MyUpdatingComponent>("#counter"); // add the updating component with the CSS selector "#updating-component"
-
+builder.RootComponents.Add<ll.MyUpdatingComponent>("updating-element"); // add the updating component with the CSS selector "#updating-component"
+;
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(ss => new MyUpdatingComponent());
 
-var host = builder.Build();
+var host = builder.Build(); // Build the host
 
 // Start the updating component
 var updatingComponent = host.Services.GetRequiredService<ll.MyUpdatingComponent>();
-updatingComponent.StartUpdating();
+
 
 await host.RunAsync();
