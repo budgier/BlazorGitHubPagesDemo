7f2149b1abde7fece62d1d8ee9a6f4c37576c302
Pavol Bakoň <pavol.bakon@atos.net>
2023-05-05 15:19:55 +0200

    odtrsani handler errpr

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ using ll;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.RootComponents.Add<ll.MyUpdatingComponent>("updating-element"); // add the updating component with the CSS selector "#updating-component"
+//builder.RootComponents.Add<ll.MyUpdatingComponent>("updating-element"); // add the updating component with the CSS selector "#updating-component"
 ;
+
+
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(ss => new MyUpdatingComponent());
@@ -17,7 +20,7 @@ builder.Services.AddScoped(ss => new MyUpdatingComponent());
 var host = builder.Build(); // Build the host
 
 // Start the updating component
-var updatingComponent = host.Services.GetRequiredService<ll.MyUpdatingComponent>();
+//var updatingComponent = host.Services.GetRequiredService<ll.MyUpdatingComponent>();
 
 
 await host.RunAsync();
