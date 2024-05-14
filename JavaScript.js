10dc6c246625cd6121dcfaa878fbb83d21448fc9
budgier <72704231+budgier@users.noreply.github.com>
2024-05-14 09:32:37 +0000

    Deploying to gh-pages from  @ 765c6bce856fdc612dae404d1b6f68ad10fc5f4a 🚀

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+﻿window.websocketFunctions = {
+    createWebSocket: function (url) {
+        window.websocket = new WebSocket(url);
+    },
+    send: function (message) {
+        window.websocket.send(message);
+    },
+    receive: function (dotnetHelper) {
+        window.websocket.onclose = function (event) {
+            dotnetHelper.invokeMethodAsync('ReceiveMessage', event.data);
+        }
+        window.websocket.onmessage = function (event) {
+            dotnetHelper.invokeMethodAsync('ReceiveMessage', event.data);
+        };
+    }
+};
+
